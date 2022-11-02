a5cca5bafa346fc8a6abb51d56e5432c99544f91
Luan Silva <37859294+LuanLuizLLs@users.noreply.github.com>
2022-11-02 13:12:24 -0300

    Add soft delete

--- app/Models/Campaigns.php
+++ app/Models/Campaigns.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Campaigns extends Model
 {
+  use SoftDeletes;
+
   protected $table = 'campaigns';
 
   public const ID = 'id';
--- app/Models/Users.php
+++ app/Models/Users.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Users extends Model
 {
+  use SoftDeletes;
+  
   protected $table = 'users';
 
   public const ID = 'id';

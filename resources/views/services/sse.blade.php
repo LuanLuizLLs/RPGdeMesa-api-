06500291b976d95bbadd5ccc7fac3e33a9ea41d9
LuanLuizLLs <luanluiz.dev@gmail.com>
2024-10-25 20:48:58 -0300

    chore: add retry in sse service

--- resources/views/services/sse.blade.php
+++ resources/views/services/sse.blade.php
@@ -4,5 +4,6 @@ foreach ($sse as $values) {
   echo "id: ".$values['id']."\n";
   echo "event: ".$values['event']."\n";
   echo "data: ".json_decode($values['data'])."\n";
+  echo "retry: 10000\n";
   echo "\n\n";
 }
